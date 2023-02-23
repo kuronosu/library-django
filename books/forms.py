483fcc6fa433c878d2b3cc09f07a9b03176e56ab
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-02-23 17:57:27 -0500

    Base, create, list and update Interface changed

--- books/forms.py
+++ books/forms.py
@@ -6,3 +6,6 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+
+#shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+        
